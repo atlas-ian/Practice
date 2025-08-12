cd8d83adfc6fa095c322c753256f736e50cb86e7
adarshaadi06 <adarshn54688@gmail.com>
2025-08-12 10:08:41 +0530

    started practicing  arrays

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,6 @@
 
 public class Main{
     public static void main(String[] args) {
-        int [] nums = {1,2,4,6,9};
-        int target = 8;
-
-        int[] result = twosum(nums, target);
-        System.out.println("Indices: [" + result[0] + "," + result[1] + "]");
-    }
-
-    public static int[] twosum(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length ; j++) {
-                if (nums[i] + nums[j] == target){
-                    return new int[]{i, j};
-                }
-
-            }
-
+        System.out.println("Hey Welcome back!");
         }
-        return new int[] {-1, -1};
-
     }
-}
---
+++ src/arrays/access.java
@@ -0,0 +1,10 @@
+package arrays;
+
+public class access {
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 5, 6, 8, 9};
+        for(int i = 0; i < arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
